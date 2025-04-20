254fbec12fd8c86e954cfaa12d5c6c155f2da631
raulgg764-edu <raul.garzagln@uanl.edu.mx>
2025-04-19 23:35:33 -0600

    Iniciar proyecto blackjack, agregar componente carta

---
+++ src/components/Card.tsx
@@ -0,0 +1,36 @@
+import cardsImg from '../assets/cards.jpg'
+import { TCard as CardType}  from '../types';
+
+export default function Card(card: CardType) {
+
+    const getPosition = (rank: string, suit: string) => {
+        const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        const suits = ['spades', 'hearts', 'clubs', 'diamonds'];
+
+        const x: number = ranks.indexOf(rank);
+        const y: number = suits.indexOf(suit);
+        return {x, y}
+    }
+
+    const position = getPosition(card.rank, card.suit)
+    console.log(`backgroundPosition: ${position.x*100}% ${position.y*100}%`)
+
+    const scale = 0.5;
+    const root = document.querySelector(":root") as HTMLElement
+
+    root!.style.setProperty("--cardScale", scale.toString())
+
+    const style = {
+        
+        backgroundImage: `url(${cardsImg})`,
+        backgroundPosition: `-${position.x*100}% -${position.y*100}%`,
+    }
+
+
+    return (
+        <div className='card' style={style}> 
+
+        </div>
+    )
+    
+}
---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+export interface TCard{
+    rank: string,
+    suit: string
+}
+
+export interface THand{
+    cards: TCard[]
+}
+
